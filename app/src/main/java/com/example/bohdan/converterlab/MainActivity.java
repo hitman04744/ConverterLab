d7e14b4792de44667e7df5bf1a3d31e9b0f0d551
Bohdan <hitman04744@gmail.com>
2015-09-21 21:42:39 +0300

    final

--- app/src/main/java/com/example/bohdan/converterlab/MainActivity.java
+++ app/src/main/java/com/example/bohdan/converterlab/MainActivity.java
@@ -1,38 +1,81 @@
 package com.example.bohdan.converterlab;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
+import com.example.bohdan.converterlab.adapter.RVAdapter;
+import com.example.bohdan.converterlab.asyncTask.AsyncResponse;
 import com.example.bohdan.converterlab.asyncTask.RequestTask;
+import com.example.bohdan.converterlab.organisationsModel.Organisations;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
 
+public class MainActivity extends AppCompatActivity implements AsyncResponse {
+    Intent intent;
+    ArrayList<Organisations>mList= new ArrayList<>();
+    RequestTask requestTask;
+    RecyclerView rv;
+    SwipeRefreshLayout mSwipeRefreshLayout;
+    RVAdapter adapter;
+    boolean status = false;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         LinearLayoutManager layoutmanager = new LinearLayoutManager(this);
 
-        RecyclerView rv = (RecyclerView)findViewById(R.id.rv);
+        startService(
+                intent = new Intent(MainActivity.this, UpdateService.class)
+        );
+
+        rv = (RecyclerView)findViewById(R.id.rv);
         rv.setLayoutManager(layoutmanager);
         rv.setItemAnimator(new DefaultItemAnimator());
 
+        adapter = new RVAdapter(this,mList);
+        rv.setAdapter(adapter);
+        requestTask = new RequestTask(this,rv);
+        requestTask.delegate = this;
+        requestTask.execute();
+        mSwipeRefreshLayout = (SwipeRefreshLayout) findViewById(R.id.swipeRefreshLayout);
+//        new RequestTask(this,rv).execute();
 
-//        RVAdapter adapter = new RVAdapter(banks);
-//        rv.setAdapter(adapter);
-        new RequestTask(this,rv).execute();
-
-
+        mSwipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                refreshItems();
+            }
+        });
 
     }
 
+    private void refreshItems() {
+        requestTask = new RequestTask(this,rv);
+        requestTask.delegate = this;
+        requestTask.execute();
+        if(status){
+            onItemsLoadComplete();
+        }
+
+    }
+
+    private void onItemsLoadComplete() {
+        adapter.notifyDataSetChanged();
+        Toast.makeText(this,"REFRESHED",Toast.LENGTH_SHORT).show();
+        mSwipeRefreshLayout.setRefreshing(false);
+        status = false;
+    }
 
 
     @Override
@@ -55,5 +98,13 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void processFinish(ArrayList<Organisations> list) {
+        this.mList = list;
+        this.status = status;
+        RVAdapter adapter = new RVAdapter(this,mList);
+        rv.setAdapter(adapter);
     }
 }
--- app/src/main/java/com/example/bohdan/converterlab/adapter/RVAdapter.java
+++ app/src/main/java/com/example/bohdan/converterlab/adapter/RVAdapter.java
@@ -1,24 +1,32 @@
 package com.example.bohdan.converterlab.adapter;
 
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.widget.CardView;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
-import com.example.bohdan.converterlab.organisationsModel.Organisations;
+import com.example.bohdan.converterlab.DetailActivity;
 import com.example.bohdan.converterlab.R;
+import com.example.bohdan.converterlab.organisationsModel.Organisations;
 
 import java.util.ArrayList;
 
 
 public class RVAdapter extends RecyclerView.Adapter<RVAdapter.BankViewHolder>  {
 
-    ArrayList<Organisations> mOrganisations;
+    private  ArrayList<Organisations> mOrganisations;
+    private Context mContext;
+    private  int position;
 
-    public RVAdapter( ArrayList<Organisations> banks){
+    public RVAdapter(Context mContext, ArrayList<Organisations> banks){
        this.mOrganisations = banks;
+        this.mContext = mContext;
     }
 
     @Override
@@ -34,10 +42,13 @@ public class RVAdapter extends RecyclerView.Adapter<RVAdapter.BankViewHolder>  {
         holder.name.setText( mOrganisations.get(position).getTitle());
         holder.location.setText( mOrganisations.get(position).getRegionId());
         holder.cityLocation.setText( mOrganisations.get(position).getCityId());
-        holder.telephone.setText( mOrganisations.get(position).getPhone());
-        holder.bankAdress.setText( mOrganisations.get(position).getAddress());
+        holder.telephone.setText(mOrganisations.get(position).getPhone());
+        holder.bankAdress.setText(mOrganisations.get(position).getAddress());
 
     }
+
+
+
 
     @Override
     public int getItemCount() {
@@ -45,28 +56,82 @@ public class RVAdapter extends RecyclerView.Adapter<RVAdapter.BankViewHolder>  {
         return  mOrganisations.size();
     }
 
+    public int getPosition() {
+        return position;
+    }
+
+    public void setPosition(int position) {
+        this.position = position;
+    }
+
     @Override
     public void onAttachedToRecyclerView(RecyclerView recyclerView) {
         super.onAttachedToRecyclerView(recyclerView);
     }
 
-    public static  class BankViewHolder extends RecyclerView.ViewHolder {
+
+
+    public static  class BankViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
         CardView cv;
         private TextView name;
         private TextView location;
         private TextView cityLocation;
         private TextView telephone;
         private TextView bankAdress;
+        private ImageButton mDetail;
+        private ImageButton mPhone;
+        private ImageButton mMap;
+        private ImageButton mLink;
 
         public BankViewHolder(View itemView) {
             super(itemView) ;
+            ArrayList<Organisations> mList = new ArrayList<>();
             cv = (CardView) itemView.findViewById(R.id.card_view);
             name =  (TextView) itemView.findViewById(R.id.info_text);
             location = (TextView) itemView.findViewById(R.id.info_text2);
             cityLocation = (TextView) itemView.findViewById(R.id.info_text3) ;
             telephone= (TextView) itemView.findViewById(R.id.info_text4);
             bankAdress = (TextView) itemView.findViewById(R.id.info_text5);
+            mDetail = (ImageButton) itemView.findViewById(R.id.next);
+            mPhone = (ImageButton) itemView.findViewById(R.id.phone);
+            mMap = (ImageButton) itemView.findViewById(R.id.map);
+            mLink = (ImageButton) itemView.findViewById(R.id.link);
+
+            mDetail.setOnClickListener(this);
+            mPhone.setOnClickListener(this);
+            mMap.setOnClickListener(this);
+            mLink.setOnClickListener(this);
+
         }
 
+        @Override
+        public void onClick(View v) {
+            switch (v.getId()){
+                case (R.id.next):
+                    Intent showDetails = new Intent(itemView.getContext(), DetailActivity.class);
+                    showDetails.putExtra("name", name.getText().toString());
+
+                    itemView.getContext().startActivity(showDetails);
+                    break;
+                case (R.id.phone):
+                    Intent callIntent = new Intent(Intent.ACTION_DIAL);
+                    callIntent.setData(Uri.parse("tel:" + telephone.getText()));
+                    itemView.getContext().startActivity(callIntent);
+
+                    break;
+                case (R.id.link):
+                    Intent linkIntent = new Intent(Intent.ACTION_VIEW);
+                    linkIntent.setData(Uri.parse("http:www.google.com"));
+                    itemView.getContext().startActivity(linkIntent);
+
+                    break;
+                case (R.id.map):
+
+                    break;
+            }
+
+
+
+        }
     }
 }
